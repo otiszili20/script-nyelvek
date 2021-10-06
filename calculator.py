189be9e57149b2bbd14fb7bb2de65609d95eeb16
otiszili20 <otiszili200@gmail.com>
2021-10-06 12:35:41 +0200

    numpy feltelepitve

---
+++ calculator.py
@@ -0,0 +1,10 @@
+print("calculator application. please give me the  ")
+
+print("1.operand")
+operand1 = int(input())
+
+print("operator")
+operator = input()
+
+print("2.operand")
+operand2 = int(input())
--- first.py
+++ first.py
@@ -1,6 +1,8 @@
-age = 8
-print(age)
+print("Pls give me your name")
+name = input()
 
-name = "hello"
-print(name)
+print("Pls give me your age")
+age = int(input())
 
+print("Your name: {0}, age: {1}".format(name, age))
+print("Next year you will be {0}".format(age + 1))
