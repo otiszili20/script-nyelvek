c18a64571e61d60add91c1b466818f617554a050
otiszili20 <otiszili200@gmail.com>
2021-10-06 19:43:01 +0200

    calculator

--- calculator.py
+++ calculator.py
@@ -1,16 +1,45 @@
-print("calculator application. please give me the  ")
 
-print("1.operand")
-operand1 = int(input())
+def ask():
+    print("calculator application. please give me the  ")
 
-print("operator")
-operator = input()
+    print("1.operand (integer)")
+    text = input()
+    while not text.isnumeric():
+        print("bad input, try again")
+        text = input()
+    operand1 = int(text)
 
-print("2.operand")
-operand2 = int(input())
+    print("operator (+ | - | * | /)")
+    text = input()
+    while text not in ['+', '-', '*', '/']:
+        print("bad input, try again")
+        text = input()
+    l_operator = text
 
-if operator == "+":
-    result = operand1 + operand2
+    print("2.operand (integer)")
+    text = input()
+    while not text.isnumeric():
+        print("bad input, try again")
+        text = input()
+    operand2 = int(text)
 
+    return operand1, l_operator, operand2
+
+
+def calculate(operand1, p_operator, operand2):
+    rv = 0
+    if p_operator == "+":
+        rv = operand1 + operand2
+    elif p_operator == "-":
+        rv = operand1 + operand2
+    elif p_operator == "*":
+        rv = operand1 * operand2
+    elif p_operator == "/":
+        rv = operand1 / operand2
+    return rv
+
+
+op1, operator, op2 = ask()
+result = calculate(op1, operator, op2)
 print(f"result: {result}")
 exit(0)
