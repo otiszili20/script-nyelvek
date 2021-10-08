b4b9245e49d1d17f06d67bc9fa1a938b4348b072
otiszili20 <otiszili200@gmail.com>
2021-10-08 08:28:57 +0200

    calculator

---
+++ 01/check.py
@@ -0,0 +1,8 @@
+import secret_logic
+
+assert not secret_logic.is_numeric("asd"), "is_numeric with not numeric"
+assert secret_logic.is_numeric("123"), "is_numeric with not numeric"
+
+assert secret_logic.is_supported_operator("%"), "is_supported_operator not supported operator"
+
+assert secret_logic.calculate(1, "+", 6) == 7, "calculate + operator"
